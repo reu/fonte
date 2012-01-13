38c849058f67397f0f60bcde5966d7c1a9d3fcb3
Rodrigo Navarro <rnavarro1@gmail.com>
2012-01-13 10:11:35 -0200

    Adding player parser

--- lib/fonte/parsers.rb
+++ lib/fonte/parsers.rb
@@ -1,6 +1,6 @@
 module Fonte
   module Parsers
-    %w(number word temporal address rcon log).each do |parser|
+    %w(number word temporal address rcon player log).each do |parser|
       Treetop.load File.join(File.dirname(__FILE__), "parsers", parser)
     end
   end
---
+++ spec/parsers/player_parser_spec.rb
@@ -0,0 +1,32 @@
+require "spec_helper"
+
+module Fonte
+  module Parsers
+    describe PlayerParser do
+      let(:parser) { described_class.new }
+      let(:player) { "Reu<2><STEAM_1:1:24968171><Red>" }
+
+      subject { parser.parse(player) }
+
+      its(:"nickname.value") { should == "Reu" }
+      its(:"uid.value")      { should == 2 }
+      its(:"steam_id.value") { should == "STEAM_1:1:24968171" }
+      its(:"team.value")     { should == "Red" }
+
+      context "when the player is a bot" do
+        let(:player) { "Nick<42><Bot><Red>" }
+        its(:"steam_id.value") { should be_nil }
+      end
+
+      context "when the team is not present" do
+        let(:player) { "Reu<2><STEAM_1:1:24968171><>" }
+        its(:"team.value") { should be_nil }
+      end
+
+      context "when the team is unissiged" do
+        let(:player) { "Reu<2><STEAM_1:1:24968171><Unassigned>" }
+        its(:"team.value") { should be_nil }
+      end
+    end
+  end
+end
